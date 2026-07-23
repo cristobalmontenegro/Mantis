b0b98cfeab68e55c8b623dfa76a05cca0acbb4a1
cristobalmontenegro <cmc@socorropc.com>
2026-07-22 21:42:12 -0500

    CustomReports: add config defaults for admin_threshold and view_custom_reports_threshold
    - admin_threshold defaults to ADMINISTRATOR (was missing, causing Warning #100)
    - view_custom_reports_threshold defaults to DEVELOPER
    - Fixes APPLICATION WARNING about missing plugin_CustomReports_admin_threshold

--- plugins/CustomReports/CustomReports.php
+++ plugins/CustomReports/CustomReports.php
@@ -16,6 +16,13 @@ class CustomReportsPlugin extends MantisPlugin {
         );
     }
 
+    function config() {
+        return array(
+            'admin_threshold' => ADMINISTRATOR,
+            'view_custom_reports_threshold' => DEVELOPER,
+        );
+    }
+
     function init() {
         require_once( __DIR__ . DIRECTORY_SEPARATOR . 'CustomReports.API.php' );
         require_once( __DIR__ . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . 'html_api.php' );
