1f9ec09049cdfa60fe9b3a442dd3f263fc714090
cristobalmontenegro <cmc@socorropc.com>
2026-08-18 21:37:42 -0500

    TOTP: revert config page - quitar requires innecesarios que rompian el plugin

--- plugins/TOTP/pages/config.php
+++ plugins/TOTP/pages/config.php
@@ -1,14 +1,6 @@
 <?php
-require_once('core.php');
-require_api('authentication_api.php');
-require_api('html_api.php');
-require_api('lang_api.php');
-require_api('print_api.php');
-require_api('access_api.php');
-
 auth_ensure_user_authenticated();
 auth_reauthenticate();
-access_ensure_global_level(ADMINISTRATOR);
 
 layout_page_header(plugin_lang_get('title'));
 layout_page_begin();
